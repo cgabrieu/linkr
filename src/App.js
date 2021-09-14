48b39f72eb1207e17cdf2e76bf649548b7d4cf96
Carlos Gabriel <carlosgabrielpc@live.com>
2021-09-14 19:22:35 -0300

    Added globalstyle, reset css & fonts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from 'react';
+import GlobalStyle from "./styles/global";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Timeline from './routes/private/Timeline';
+
 function App() {
   return (
-    <>
-      <h1>Linkr</h1>
-    </>
+    <Router>
+      <GlobalStyle />
+      <Switch>
+        <Route exact path='/timeline' component={Timeline} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/styles/global.js
@@ -0,0 +1,10 @@
+import "./reset.css"
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    body {
+        //font-family: 'Lato', sans-serif;
+        //font-family: 'Passion One', cursive;
+        //font-family: 'Lato', sans-serif;
+    }
+`;
