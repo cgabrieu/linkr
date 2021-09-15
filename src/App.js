7eb4b05d1dd21eeee138fcd05a3708168ec7b87a
Marcio <marcioabcd11@gmail.com>
2021-09-15 17:53:43 -0300

    Refactoring expandable menu without using DOM

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GlobalStyle from "./styles/global";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Timeline from './routes/private/Timeline';
 import Header from './components/header/Header';
 
 function App() {
+  const [clicked, setClicked] = useState(false)
+
   return (
-    <Router>
-      <GlobalStyle />
-      <Header />
-      <Switch>
-        <Route exact path='/timeline' component={Timeline} />
-      </Switch>
-    </Router>
+    <div onClick={() => setClicked(false)}>
+      <Router>
+        <GlobalStyle />
+        <Header clicked={clicked} setClicked={setClicked} />
+        <Switch>
+          <Route exact path='/timeline' component={Timeline} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,45 +1,26 @@
-import { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import arrow from '../../assets/HeaderArrow.svg';
 import { HeaderBar, Tittle, ArrowAndPhoto, Arrow, Photo, ExpandableMenu } from "./HeaderStyle";
 
-export default function Header() {
-    const [clicked, setClicked] = useState(false);
-    const node = useRef();
+export default function Header(props) {
+    const { clicked, setClicked } = props;
 
-    function toggleAppearingBox() {
-        if (clicked) {
-            setClicked(false);
-        } else {
-            setClicked(true);
-        }
+    function toggleClicked(e){
+        clicked ? setClicked(false) : setClicked(true);
+        e.stopPropagation();
     }
-
-    const handleClick = e => {
-        if (node.current.contains(e.target)) {
-            return;
-        }
-        setClicked(false);
-    };
-
-    useEffect(() => {
-        document.addEventListener("mousedown", handleClick);
-        return () => {
-            document.removeEventListener("mousedown", handleClick);
-        };
-    }, []);
 
     return (
         <HeaderBar>
             <Tittle>linkr</Tittle>
             <ArrowAndPhoto>
-                <Arrow onClick={toggleAppearingBox} clicked={clicked} src={arrow} alt='Arrow to show expandable menu' />
-                <Photo onClick={toggleAppearingBox} src='https://img.ibxk.com.br/2017/06/22/22100428046161.jpg?w=1200&h=675&mode=crop&scale=both' alt='User photo' />
+                <Arrow onClick={(event) => toggleClicked(event)} clicked={clicked} src={arrow} alt='Arrow to show expandable menu' />
+                <Photo onClick={(event) => toggleClicked(event)} src='https://img.ibxk.com.br/2017/06/22/22100428046161.jpg?w=1200&h=675&mode=crop&scale=both' alt='User photo' />
             </ArrowAndPhoto>
-            <ExpandableMenu clicked={clicked} ref={node}>
-                <Link to='/my-posts' onClick={toggleAppearingBox}><p>My posts</p></Link>
-                <Link to='/my-likes' onClick={toggleAppearingBox}><p>My likes</p></Link>
-                <Link to='/' onClick={toggleAppearingBox}><p>Logout</p></Link>
+            <ExpandableMenu clicked={clicked}>
+                <Link onClick={(event) => toggleClicked(event)} to='/my-posts' ><p>My posts</p></Link>
+                <Link onClick={(event) => toggleClicked(event)} to='/my-likes' ><p>My likes</p></Link>
+                <Link onClick={(event) => toggleClicked(event)} to='/' ><p>Logout</p></Link>
             </ExpandableMenu>
         </HeaderBar>
     )
