777e5047fbe1a1350c18d509cdbf83890583d164
Samuel Felipe <samuelfelipef@gmail.com>
2021-09-15 18:59:13 -0300

    add login route layout

--- src/routes/public/LoginRoute/LoginRoute.js
+++ src/routes/public/LoginRoute/LoginRoute.js
@@ -1,6 +1,41 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Loading from "../../../components/Loading";
+import { PageContent, Aside, Main, Input, Button } from "../LoginAndSignUpStyles";
+
 function LoginRoute() {
+
+  const [btnDisabled, setBtnDisabled] = useState(false);
+
   return (
-    <h1>Login</h1>
+    <PageContent>
+      <Aside>
+        <h1>linkr</h1>
+        <h3>save, share and discover the best links on the web</h3>
+      </Aside>
+
+      <Main>
+        <form onSubmit={""}>
+          <Input
+            type="email"
+            name="email"
+            placeholder="e-mail" />
+          <Input
+            type="password"
+            name="password"
+            placeholder="password" />
+          <Button disabled={false}>
+            {!btnDisabled
+              ? "Log In"
+              : <Loading visible={true} />
+            }
+          </Button>
+        </form>
+        <Link to="/sign-up">
+          <span>Firts time? Create an account!</span>
+        </Link>
+      </Main>
+    </PageContent>
   );
 }
 
