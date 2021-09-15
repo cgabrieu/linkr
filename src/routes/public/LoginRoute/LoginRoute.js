bf600ce3b589a396ac4f4626175f79547e372e36
Samuel Felipe <samuelfelipef@gmail.com>
2021-09-15 14:38:47 -0300

    add login page layout desktop and mobile

--- src/routes/public/LoginRoute/LoginRoute.js
+++ src/routes/public/LoginRoute/LoginRoute.js
@@ -1,7 +1,36 @@
-function LoginRoute() {
+import { Link } from "react-router-dom";
+import { Aside, Button, Input, Main, PageContent } from "../LoginAndSignUpStyles";
+
+function LogInRoute() {
+
+
+
   return (
-    <h1>Login</h1>
+    <PageContent>
+      <Aside>
+        <h1>linkr</h1>
+        <h3>save, share and discover the best links on the web</h3>
+      </Aside>
+
+      <Main>
+        <form>
+          <Input
+            type="email"
+            name="email"
+            placeholder="email"
+          />
+          <Input
+            type="password"
+            name="password"
+            placeholder="password" />
+          <Button>Log In</Button>
+        </form>
+        <Link to="/sign-up">
+          <span>First time? Create an account!</span>
+        </Link>
+      </Main>
+    </PageContent>
   );
 }
 
-export default LoginRoute;
+export default LogInRoute;
