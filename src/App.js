a1af3ddb4239780d250cb0f7fd94562f2e86e4d3
Samuel Felipe <samuelfelipef@gmail.com>
2021-09-16 10:29:27 -0300

    fixed: redirect user to /

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import UserContext from "./contexts/UserContext";
 import GlobalStyle from "./styles/global";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Timeline from './routes/private/Timeline';
 import SignUpRoute from './routes/public/SingUpRoute/SignUpRoute';
 import LogInRoute from './routes/public/LoginRoute/LoginRoute';
@@ -18,7 +18,7 @@ function App() {
           <Route exact path="/sign-up" component={SignUpRoute} />
           <Route exact path='/timeline' component={Timeline} />
           <Route exact path="/" component={LogInRoute} />
-          <Route exact path="*" component={LogInRoute} />
+          <Redirect to="/" />
         </Switch>
       </UserContext.Provider>
     </Router>
