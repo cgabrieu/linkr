1a10d43ee08f9bef266abd0c4a19f36eca814ffd
Marcio <marcioabcd11@gmail.com>
2021-09-16 11:08:51 -0300

    Showing photo and reseting local storage when clicking logout

--- src/App.js
+++ src/App.js
@@ -1,57 +1,33 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import UserContext from "./contexts/UserContext";
-=======
->>>>>>> feat/header
-=======
->>>>>>> feat/header
+import ExpandableMenuContext from './contexts/ExpandableMenuContext';
 import GlobalStyle from "./styles/global";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Timeline from './routes/private/Timeline';
 import Header from './components/header/Header';
 import SignUpRoute from './routes/public/SingUpRoute/SignUpRoute';
 import LogInRoute from './routes/public/LoginRoute/LoginRoute';
-import ExpandableMenuContext from './contexts/ExpandableMenuContext';
 
 function App() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-
   const [user, setUser] = useState("");
-=======
   const [isExpandableMenuOpen, setIsExpandableMenuOpen] = useState(false);
->>>>>>> feat/header
-=======
-  const [isExpandableMenuOpen, setIsExpandableMenuOpen] = useState(false);
->>>>>>> feat/header
 
   return (
-
     <Router>
-<<<<<<< HEAD
-      <GlobalStyle />
-      <Switch>
-        <ExpandableMenuContext.Provider value={{isExpandableMenuOpen, setIsExpandableMenuOpen}}>
-          <div onClick={() => setIsExpandableMenuOpen(false)}>
-            <Header />
-            <Route exact path="/" component={LogInRoute} />
-            <Route exact path="/sign-up" component={SignUpRoute} />
-            <Route exact path='/timeline' component={Timeline} />
-          </div>
-        </ExpandableMenuContext.Provider>
-      </Switch>
-=======
       <UserContext.Provider value={{ user, setUser }}>
         <GlobalStyle />
         <Switch>
-          <Route exact path="/sign-up" component={SignUpRoute} />
-          <Route exact path='/timeline' component={Timeline} />
-          <Route exact path="/" component={LogInRoute} />
-          <Redirect to="/" />
+          <ExpandableMenuContext.Provider value={{ isExpandableMenuOpen, setIsExpandableMenuOpen }}>
+            <div onClick={() => setIsExpandableMenuOpen(false)}>
+              <Route exact path="/sign-up" component={SignUpRoute} />
+              <Route exact path='/timeline' component={Timeline} />
+              <Route exact path="/" component={LogInRoute} />
+              <Redirect to="/" />
+            </div>
+          </ExpandableMenuContext.Provider>
         </Switch>
+
       </UserContext.Provider>
->>>>>>> 001988f7f87e3f96f2e1c7c7edf76bb724089f66
     </Router>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,9 +3,17 @@ import arrow from '../../assets/HeaderArrow.svg';
 import { HeaderBar, Tittle, ArrowAndPhoto, Arrow, Photo, ExpandableMenu } from "./HeaderStyle";
 import { useContext } from 'react';
 import ExpandableMenuContext from '../../contexts/ExpandableMenuContext';
+import UserContext from '../../contexts/UserContext';
 
 export default function Header() {
     const { isExpandableMenuOpen, setIsExpandableMenuOpen } = useContext(ExpandableMenuContext);
+    const { user } = useContext(UserContext);
+    const avatar = user.user.avatar;
+
+    function cleanUserInfo(e) {
+        localStorage.clear();
+        toggleOpen(e);
+    }
 
     function toggleOpen(e) {
         isExpandableMenuOpen ? setIsExpandableMenuOpen(false) : setIsExpandableMenuOpen(true);
@@ -17,12 +25,12 @@ export default function Header() {
             <Tittle>linkr</Tittle>
             <ArrowAndPhoto>
                 <Arrow onClick={(event) => toggleOpen(event)} isExpandableMenuOpen={isExpandableMenuOpen} src={arrow} alt='Arrow to show expandable menu' />
-                <Photo onClick={(event) => toggleOpen(event)} src='https://img.ibxk.com.br/2017/06/22/22100428046161.jpg?w=1200&h=675&mode=crop&scale=both' alt='User photo' />
+                <Photo onClick={(event) => toggleOpen(event)} src={avatar} alt='User photo' />
             </ArrowAndPhoto>
             <ExpandableMenu onClick={(event) => event.stopPropagation()} isExpandableMenuOpen={isExpandableMenuOpen}>
                 <Link onClick={(event) => toggleOpen(event)} to='/my-posts' ><p>My posts</p></Link>
                 <Link onClick={(event) => toggleOpen(event)} to='/my-likes' ><p>My likes</p></Link>
-                <Link onClick={(event) => toggleOpen(event)} to='/' ><p>Logout</p></Link>
+                <Link onClick={(event) => cleanUserInfo(event)} to='/' ><p>Logout</p></Link>
             </ExpandableMenu>
         </HeaderBar>
     )
